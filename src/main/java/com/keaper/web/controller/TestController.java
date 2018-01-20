cddab3f49bd98c051b322183aa5d9f47a62c79a6
edward <1273570695@qq.com>
2018-01-21 01:45:59 +0800

    spring mvc config

---
+++ src/main/java/com/keaper/web/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.keaper.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+
+}
