08eb156ffc3f0c7b57ed4b597f90719d9bc55894
edward <1273570695@qq.com>
2018-02-04 02:00:48 +0800

    init react

---
+++ front/webpack.dev.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+
+    entry:path.join(__dirname,'src/index.js'),
+    output:{
+        path:path.join(__dirname,'./dist'),
+        filename:'bundle.js'
+    },
+
+    devtool: 'inline-source-map',
+
+    module:{
+        rules: [{
+            test: /\.js$/,
+            use:['babel-loader?cacheDirectory=true'],
+            include:path.join(__dirname,'./src/')
+        }]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+    },
+};
